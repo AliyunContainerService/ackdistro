1befacc05d645f056b9bab5d4ed68761d4c868d4
huaiyou <huaiyou.cyz@alibaba-inc.com>
2022-05-07 16:43:22 +0800

    Stash this

--- build/build.sh
+++ build/build.sh
@@ -8,16 +8,22 @@ if ! which sealer;then
 fi
 
 TAG=$1
+ARCH=$2
 
 if [[ "$TAG" == "" ]];then
     echo "Usage: bash build.sh TAG"
     exit 1
 fi
 
+if [[ "$ARCH" == "" ]];then
+    echo "ARCH is not set, default is amd64, if you want build other arch, please run bash build.sh TAG ARCH"
+    ARCH=amd64
+fi
+
 bins=(helm kubectl kubelet kubeadm trident)
 for bin in ${bins[@]};do
-    wget https://acs-ecp.oss-cn-hangzhou.aliyuncs.com/ack-distro/bin/amd64/${bin} -O ${bin}
+    wget https://acs-ecp.oss-cn-hangzhou.aliyuncs.com/ack-distro/bin/${ARCH}/${bin} -O ${bin}
 done
 
 # Build sealer image
-sealer build -m lite -t ack-distro:${TAG} .
+sealer build -m lite -t ack-distro:${TAG} --platform ${ARCH} .
