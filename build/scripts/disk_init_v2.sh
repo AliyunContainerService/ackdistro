a769e01d08f60b5f2ef866583dcaa1ee2c6c581e
huaiyou <huaiyou.cyz@alibaba-inc.com>
2022-07-26 17:16:10 +0800

    Improve lvcreate.
    Signed-off-by: huaiyou <huaiyou.cyz@alibaba-inc.com>

--- build/scripts/disk_init_v2.sh
+++ build/scripts/disk_init_v2.sh
@@ -120,7 +120,7 @@ kubelet_size=$kubelet_size"Gi"
 
 lvs|grep $lv_container_name
 if [ "$?" != "0" ]; then
-    output1=$(lvcreate --name $lv_container_name --size $container_runtime_size $vgName -y 2>&1)
+    output1=$(lvcreate -Z --name $lv_container_name --size $container_runtime_size $vgName -y 2>&1)
     if [ "$?" != "0" ]; then
         panic "failed to create $lv_container_name lv: $output1"
     fi
@@ -130,7 +130,7 @@ fi
 
 lvs|grep $lv_kubelet_name
 if [ "$?" != "0" ]; then
-    output2=$(lvcreate --name $lv_kubelet_name --size $kubelet_size $vgName -y 2>&1)
+    output2=$(lvcreate -Z --name $lv_kubelet_name --size $kubelet_size $vgName -y 2>&1)
     if [ "$?" != "0" ]; then
         panic "failed to create $lv_kubelet_name lv: $output2"
 ]    fi
