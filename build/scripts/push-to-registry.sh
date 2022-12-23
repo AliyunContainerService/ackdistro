58f29cc82b5eab9a58c6f43127a36742177254fa
huaiyou <huaiyou.cyz@alibaba-inc.com>
2022-12-23 21:38:52 +0800

    Support deployMode,gatewayDomain,registryExternalURL; Add push-to-registry.sh etcdctl_v3.sh
    Signed-off-by: huaiyou <huaiyou.cyz@alibaba-inc.com>

---
+++ build/scripts/push-to-registry.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+set -e
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+  echo "此脚本的入参为一个本地存在的docker镜像的tar包路径或者一个本地存在的docker镜像名"
+  echo "此脚本会将传入的镜像tar包或者镜像名，转存到sealer registry当中"
+  echo "Usage: $0 /root/image.tar #这是一个docker镜像save的tar包"
+  echo "       $0 image.tgz #这是对一个docker镜像save的tar包进行gzip压缩的包"
+  echo "       $0 nginx:latest #这是一个docker镜像名称"
+  exit 0
+fi
+
+split_image_name() {
+  ImageUrl=$1
+  res="${ImageUrl//[^\/]}"
+  PartNum=${#res}
+  if [ ${PartNum} -eq 2 ];then
+    Domain=$(echo $ImageUrl | cut -d'/' -f 2)
+    Image=$(echo $ImageUrl | cut -d'/' -f 3)
+  elif [ ${PartNum} -eq 1 ];then
+    Domain=default
+    Image=$(echo $ImageUrl | cut -d'/' -f 2)
+  elif [ ${PartNum} -eq 0 ];then
+    Domain=default
+    Image=$ImageUrl
+  fi
+}
+
+if echo "$1" | grep -q -E '\.tar$';then
+  FullName=`docker load -i ${1} | cut -d' ' -f 3`
+elif echo "$1" | grep -q -E '\.tgz$';then
+  image=`tar -xvf $1`
+  FullName=`docker load -i ${image} | cut -d' ' -f 3`
+else
+  FullName="$1"
+fi
+
+split_image_name $FullName
+
+for m in `kubectl get no -owide  |grep master|awk '{print $6}'`;do
+  docker tag $ImageUrl ${m}:5000/$Domain/$Image
+  docker push ${m}:5000/$Domain/$Image
+done
+
+echo "已成功转存到 registry-internal.adp.aliyuncs.com:5000/$Domain/$Image"
