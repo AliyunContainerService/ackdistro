50ee99c058803e1d30ec36a8b75ffe89cae2633c
huaiyou <huaiyou.cyz@alibaba-inc.com>
2023-04-25 16:54:55 +0800

    Fix runc cve.
    Signed-off-by: huaiyou <huaiyou.cyz@alibaba-inc.com>

--- scp-build-dir.sh
+++ scp-build-dir.sh
@@ -1,5 +1,5 @@
 echo -n `git log -1 --pretty=format:%h` > /tmp/VERSION
 
-EIP=39.101.77.160
+EIP=8.130.78.67
 scp -r build/* root@${EIP}:/root/cnstack/build/
 scp -r /tmp/VERSION root@${EIP}:/root/cnstack/build
